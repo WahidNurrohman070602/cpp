4fb9e1ba9912c9a6d93d0cd5fa3506f82abf9a1a
Wahid Nurrohman <83296686+WahidNurrohman070602@users.noreply.github.com>
2021-05-30 21:23:23 +0700

    mencari luas segitiga

---
+++ mencari luas segitiga.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+main() {
+	float alas,tinggi,luas;
+	cout<<"masukkan data alas";
+	cin>>alas;
+	cout<<"masukkan data tinggi";
+	cin>>tinggi;
+	cout<<"luas=0,5*alas*tinggi"; <<"hasil";
+	return 0;
+}
