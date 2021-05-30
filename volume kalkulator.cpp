de618b936fcd71284508e04f1f8564f10aa5f057
Wahid Nurrohman <83296686+WahidNurrohman070602@users.noreply.github.com>
2021-05-30 21:18:30 +0700

    Volume Calculator

---
+++ volume kalkulator.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int p, l, t, hasil;
+	
+	cout<<"==============================\n";
+	cout<<"volume kalkulator\n";
+	cout<<"==============================\n";
+	cout<<"nilai panjang\n";
+	cin>>p;
+	cout<<"nilai lebar\n";
+	cin>>l;
+	cout<<"nilai tinggi\n";
+	cin>>t;
+	hasil=p*l*t;
+	cout<<"===============================\n";
+	cout<<"volume : " <<hasil;
+}
