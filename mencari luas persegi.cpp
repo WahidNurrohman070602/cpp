3834381118f89c06e7d8bd657429cab06850b0cc
Wahid Nurrohman <83296686+WahidNurrohman070602@users.noreply.github.com>
2021-05-30 21:19:59 +0700

    mencari luas segitiga

---
+++ mencari luas persegi.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<cstdlib>
+using namespace std;
+
+int main() {
+	float a,b;
+	int kode;
+	menu:
+		cout<<"kalkulator sederhana DEV C++\n";
+		cout<<"XRY-CS.id\n\n";
+		cout<<"pilihan menu:\n";
+		cout<<"1.penjumlahan (+)\n";
+		cout<<"2.pengurangan (-)\n";
+		cout<<"3.perkalian (*)\n";
+		cout<<"4.permbagian (/)\n\n";
+		cout<<"silahkan masukkan kode menu:";cin>>kode;
+		
+}
