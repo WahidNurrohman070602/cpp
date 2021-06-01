2b83759e7fef16687831a6fcbf12fba61023623e
Wahid Nurrohman <83296686+WahidNurrohman070602@users.noreply.github.com>
2021-06-01 09:40:51 +0700

    Update kalkulator c++.cpp

--- kalkulator c++.cpp
+++ kalkulator c++.cpp
@@ -7,23 +7,18 @@ int main()
 	char aritmatika;
 	
 	cout << "selamat datang di program aritmatika";
-	
-	//masukkan input dari user
 	cout <<"masukkan nilai pertama : ";
 	cin >> a;
-	cout<<"=========================================" <<endl;
-	//pilih operator aritmatika
+	cout<<"=========================================\n";
 	cout << "pilih operator +,-,/,*,: ";
 	cin >> aritmatika;
-	cout<<"=========================================" <<endl;
-	//masukkan input dari user
+	cout<<"=========================================\n";
 	cout <<"masukkan nilai kedua : ";
 	cin >> b;
-	cout<<"=========================================" <<endl;
+	cout<<"=========================================\n";
 	
 	cout <<"\nHasil perhitungan: ";
 	cout << a <<aritmatika <<b;
-	//masukkan operator aritmatika
 	if (aritmatika == '+'){
 		hasil = a+b;
 	}else if (aritmatika == '-'){
@@ -33,11 +28,9 @@ int main()
 	}else if (aritmatika == '*'){
 		hasil = a*b;	
 	}else {
-		//jika user salah memasukkan angka maka yang keluar adalah
-		cout << "operator anda salah!" << endl;
+		cout << "operator anda salah!\n";
 	}
 	
-	//hasil dari perhitungan
 	cout << "=" << hasil << endl;
 	
 	cin.get();
