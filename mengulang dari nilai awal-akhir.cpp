69f1ae2ec99626c3d33229774d04f972b3494915
Wahid Nurrohman <83296686+WahidNurrohman070602@users.noreply.github.com>
2021-06-04 15:24:18 +0700

    Add files via upload

---
+++ mengulang dari nilai awal-akhir.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<stdlib.h>
+using namespace std;
+main()
+{
+	int awal,akhir;
+	cout<<"masukkan angka awal :";
+	cin>>awal;
+	cout<<"jumlah angka akhir :";
+	cin>>akhir;
+	for(int a=awal;a<=akhir;a++)
+	{
+		cout<<"wahid nurrohman "<<". ";
+	}
+}
