17701fa8d68d78072cd7047954d2b3b6adccaeff
Wahid Nurrohman <83296686+WahidNurrohman070602@users.noreply.github.com>
2021-06-04 14:55:12 +0700

    Add files via upload

---
+++ bilangan genap dan ganjil.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<stdlib.h>
+using namespace std;
+main()
+{
+	int bil;
+	cout<<"masukkan bilangan bulat sembarang";
+	cin>>bil;
+	hasil=%2;
+	switch (hasil)
+	{
+		case 0:
+		cout<<"bilangan genap";
+		break;
+		
+		default:
+		cout<<"bilangan ganjil";
+		break;
+	}
+}
