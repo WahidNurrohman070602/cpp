842183531aa4cac036a179007fc23683d9a84564
Wahid Nurrohman <83296686+WahidNurrohman070602@users.noreply.github.com>
2021-06-09 14:46:25 +0700

    Add files via upload

---
+++ perkalian for.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+main()
+{
+	int awal, akhir;
+	cout<<"tampilkan nilai awal : ";
+	cin>>awal;
+	cout<<"tampilkan nilai akhir : ";
+	cin>>akhir;
+	for(int a=awal;a<=akhir;a++)
+	{
+		cout<<a<<", ";
+	}
+}
