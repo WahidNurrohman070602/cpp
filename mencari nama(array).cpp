20f06ee84751a73b88e50ff6098652be878068d9
Wahid Nurrohman <83296686+WahidNurrohman070602@users.noreply.github.com>
2021-06-15 11:36:46 +0700

    Add files via upload

---
+++ mencari nama(array).cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<stdio.h>
+using namespace std;
+main()
+{
+	string nama[6]={"joko","widodo","juni","maret","susilo","prabowo"};
+	{
+		cout<<nama[3]<<endl;
+	}
+	
+	char nomor[6]={'1','2','3','4','5','6'};
+	{
+		cout<<nomor[3];
+	}
+}
