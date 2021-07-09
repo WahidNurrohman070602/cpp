5021963c0135c3bbd15c0638a9ce5fbfee457b3d
Wahid Nurrohman <83296686+WahidNurrohman070602@users.noreply.github.com>
2021-07-09 15:59:16 +0700

    Add files via upload

--- manajemen keuangan umkm.cpp
+++ manajemen keuangan umkm.cpp
@@ -2,35 +2,41 @@
 #include<stdlib.h>
 using namespace std;
 main() {
+	A:
 	char ulang;
-	do {
-		int pil,modal,pendapatan,laba,rugi;
-	    cout<<"Manajemen Keuangan UMKM" <<endl;
-	    cout<<"-----------------------" <<endl;
-	    cout<<"======Pilihan Program======" <<endl;
-	    cout<<"1. Laba dan Rugi" <<endl;
-	    cout<<"-----------------------" <<endl;
-	    cout<<"Masukan pilihan anda : ";
-		cin>>pil;
-	    cout<<"-----------------------" <<endl;
-	    switch(pil) {
-	    	case 1:
-			    cout<<"Menghitung Laba dan Rugi" <<endl;
-			    cout<<"------------------------" <<endl;
-			    cout<<"Masukan jumlah modal : Rp. ";
-				cin>>modal;
-			    cout<<"Masukan jumlah pendapatan : Rp. ";
-				cin>>pendapatan;
-			    cout<<"---------------------------" <<endl;
-			    if(pendapatan>modal) {
-			    	laba=pendapatan-modal;
-			        cout<<"Anda mendapat laba sebesar : Rp." <<laba<<endl;
-			    }else if(pendapatan<modal) {
-			        rugi=pendapatan-modal;
-			        cout<<"Anda mendapat rugi sebesar : Rp." <<rugi<<endl;
-			    }else {
-			        cout<<"Anda tidak laba dan rugi" <<endl;
-			    }
-		    }
-	}while(ulang=='y' || ulang=='Y');
+	int pil,modal,pendapatan,laba,rugi;
+	system("cls");
+    cout<<"Manajemen Keuangan UMKM" <<endl;
+	cout<<"--------------------------------" <<endl;
+	cout<<"=========Pilihan Program========" <<endl;
+	cout<<"1. Laba dan Rugi" <<endl;
+	cout<<"--------------------------------" <<endl;
+	cout<<"Masukan pilihan anda : ";
+	cin>>pil;
+	cout<<"--------------------------------" <<endl;
+	switch(pil) {
+	    case 1:
+			cout<<"Menghitung Laba dan Rugi" <<endl;
+			cout<<"--------------------------------" <<endl;
+			cout<<"Masukan jumlah modal : Rp. ";
+			cin>>modal;
+			cout<<"Masukan jumlah pendapatan : Rp. ";
+			cin>>pendapatan;
+			cout<<"--------------------------------" <<endl;
+			if(pendapatan>modal) {
+			    laba=pendapatan-modal;
+			    cout<<"Anda mendapat laba sebesar : Rp." <<laba<<endl;
+			}else if(pendapatan<modal) {
+			    rugi=pendapatan-modal;
+			    cout<<"Anda mendapat rugi sebesar : Rp." <<rugi<<endl;
+			}else {
+			    cout<<"Anda tidak laba dan rugi" <<endl;
+			}
+	}
+	cout<<"--------------------------------" <<endl;
+	cout<<"apakah anda ingin mengulang ? (Y/N) : ";
+	cin>>ulang;
+	if(ulang=='y' || 'Y') {
+		goto A;
+	}
 }
