e214b04ed02b21ca416229bc5fa15f1f444398a8
Wahid Nurrohman <83296686+WahidNurrohman070602@users.noreply.github.com>
2021-10-13 13:26:12 +0700

    Add files via upload

---
+++ tugas algoritma memasukkan nama,mencari nilai,jumlah nilai dan rata-rata.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+main() {
+	string nama[5];
+	int nilai[5];
+	int a=0,sum=0;
+	float jumlah_data,rata_rata;
+	jumlah_data=sizeof(nilai)/sizeof(*nilai);
+	for(a=0;a<5;a++) {
+		cout<<"masukkan nama : ";
+		cin>>nama[a];
+		cout<<"masukkan nilai : ";
+		cin>>nilai[a];
+	}
+	cout<<"\n============ | output data | ============\n";
+	cout<<endl;	
+	for(a=0;a<5;a++) {
+		cout<<"nama : " <<nama[a] <<endl;
+		cout<<"nilai : " <<nilai[a] <<endl;
+		sum+=nilai[a];
+	}
+	cout<<"\n============ | output nilai | ============\n";
+	cout<<endl;
+	rata_rata=sum/jumlah_data;
+	cout<<"jumlah nilai : " <<sum<<endl;
+	cout<<"rata-rata nilai : " <<rata_rata;
+}
